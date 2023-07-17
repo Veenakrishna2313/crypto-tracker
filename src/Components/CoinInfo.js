d13220d5d05857147bfa95057c5bdca13e2fed12
VeenaKrishna2313 <veenakrishna.s@gmail.com>
2023-07-16 22:53:45 -0700

    Coin Info created

--- src/Components/CoinInfo.js
+++ src/Components/CoinInfo.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react';
+import { CryptoState } from "../CryptoContext";
+import axios from "axios";
+import {HistoricalChart} from "../Config/api"
 
-const CoinInfo = () => {
+const CoinInfo = ({coin}) => {
+
+  const [historicalData,setHistoricalData]=useState();
+  const [days,setDays]=useState(1);
+
+  const {currency}=CryptoState();
+
+    useEffect(() => {
+      const fetchHistoricData = async () => {
+        const { data } = await axios.get(HistoricalChart(coin.id, days,currency));
+        console.log("Historic data", data);
+
+       setHistoricalData(data.prices);
+      };
+
+      fetchHistoricData(); 
+    }, [currency, days]);
+
   return (
     <div>CoinInfo</div>
   )
